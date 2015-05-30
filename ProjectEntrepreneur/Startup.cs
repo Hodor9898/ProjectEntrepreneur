5441367284d74e04ec9c6ada2eb37567649d1206
smurf <mbytester9898@gmail.com>
2015-05-30 16:57:27 +0300

    commit all files

---
+++ ProjectEntrepreneur/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectEntrepreneur.Startup))]
+namespace ProjectEntrepreneur
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
